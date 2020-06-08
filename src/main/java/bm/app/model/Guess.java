3923a5ec89e36c2653527ae7a91915d01218022b
Dazikiri Anikar <mierzwinski.b@gmail.com>
2020-06-08 22:46:11 +0200

    added a database's connection

---
+++ src/main/java/bm/app/model/Guess.java
@@ -0,0 +1,37 @@
+package bm.app.model;
+
+public class Guess {
+
+    private int id;
+    private String word;
+    private int number;
+
+    public Guess(String word, int number){
+        this.word = word;
+        this.number = number;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
